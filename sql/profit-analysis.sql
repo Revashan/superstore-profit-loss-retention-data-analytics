460b98ea10a231c805a55fce0c0980eaaf5ef25f
Revashan <141326969+Revashan@users.noreply.github.com>
2026-03-10 14:51:56 +0800

    Update profit-analysis.sql

--- sql/profit-analysis.sql
+++ sql/profit-analysis.sql
@@ -6,3 +6,13 @@ SELECT
 FROM superstore_sales
 GROUP BY category
 ORDER BY total_profit DESC;
+
+/* Top Profitable products */
+
+SELECT
+    product_name,
+    SUM(profit) AS total_profit
+FROM superstore_sales
+GROUP BY product_name
+ORDER BY total_profit DESC
+LIMIT 10;
